ad0d1d5165fbc74d4d7f81607bffabd7e708aa2f
Adam Liewehr <al8425b@american.edu>
2026-01-25 14:44:00 -0500

    RECURSION AHHHHHHH

---
+++ chatper1/exercises/Factorial.hs
@@ -0,0 +1,10 @@
+module Factorial where
+
+factorial n = 
+    if n == 1
+    then 1
+    else
+        if n==0
+        then 1
+        else    
+            n * factorial (n-1)
---
+++ chatper1/exercises/Fib.hs
@@ -0,0 +1,3 @@
+module Fib where
+
+    
